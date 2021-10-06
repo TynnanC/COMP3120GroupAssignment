08137d8b3d699b837adb8836f160b96764cced72
TynnanC <87954601+TynnanC@users.noreply.github.com>
2021-10-06 17:57:45 +1100

    Use links to navigate to a trainer/client profile

--- src/App.js
+++ src/App.js
@@ -1,61 +1,44 @@
 import React, { useState, useEffect } from "react";
 import Sidebar from "./components/Sidebar";
 import "./styles/App.css";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import TrainerProfile from './components/TrainerProfile.js';
+import ClientProfile from './components/ClientProfile.js';
+
+const data={
+  trainer:
+      {
+          "id":0,
+          "trainerName": "John Smith",
+          "clientsId":[0]
+      },
+      client:{
+        "id":0,
+        "name":"Bob Bobalooba",
+        "password":"bob",
+        "trainerId":0
+      } 
+}
 
 const App = () => {
   return (
-    <div className='App'>
-      <header className="App-header">
-      <p>COMP3120: Advanced Web Development</p>
-      </header>
-      {/* use a table to make the display neater */}
-      <table className='App-table'>
-        <thead>
-          <tr>
-        <th>Unit Code</th>
-        <th>Unit Title</th>
-        <th>Unit Offered</th>
-        </tr>
-        </thead>
-        <tbody>
-      {/* Use map method to work with array */}
-      {units.map(item => (
-        <Unit key = {item.id} unitCode = {item.code} unitTitle = {item.title} unitOffering = {item.unitOffering}/>
-      ))}
-      </tbody>
-      </table>
-      <form onSubmit={addUnit}>
-         <input value={newUnitCode}
-         onChange={handleNewUnitCodeChange}/>
-         <input
-         value={newUnitTitle}
-         onChange={handleNewUnitTitleChange}/>
-         <input
-         value={newUnitOffering}
-         onChange={handleNewUnitOfferingChange}/>
-         <button type="submit"> Add Unit </button>
-       </form>
-
-        {/*Below is the idea of how the profiles will be reached from the home screen*/}
-       {/*
+    <div>
+      {/*Below is the idea of how the profiles will be reached from the home screen*/}
        <Router>
+         <div>
+           <Link to="/api/trainer/0">Trainer</Link>
+           <Link to="/api/client/0">Client</Link>
+           </div>
          <Switch>
-           <Route path="/">
+           <Route path="/api/trainer/0">
              <TrainerProfile trainer={data.trainer}/>
            </Route>
-           <Route path="/">
+           <Route path="/api/client/0">
              <ClientProfile client={data.client}/>
            </Route>
          </Switch>
-        </Router>*/}
-        {/*These are being displayed directly in the home screen for development purposes*/}
-        <TrainerProfile trainer={data.trainer}/>
-        <ClientProfile client={data.client}/>
-       
-  </div>
-
-  
-
+        </Router>
+      </div>
   )
 }
 
