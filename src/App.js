541c18c01381b8691a1e578c0c035a7980ea7b81
CYBERSPACE\DELL <ali.kazim@students.mq.edu.au>
2021-10-18 22:01:19 +0500

    conditional rendering in App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 import React, { useState, useEffect } from "react";
-import Sidebar from "./components/Sidebar";
+//import Sidebar from "./components/Sidebar";
 import "./styles/App.css";
-import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import ClientProfile from './components/ClientProfile.js';
 import TrainerProfile from './components/TrainerProfile.js';
-import  LoginForm from  "./components/LoginForm.js";
+//import  LoginForm from  "./components/LoginForm.js";
 import DoWorkout from "./components/DoWorkout.js";
+import Auth0Login from "./components/Auth0Login.js"
+import { useAuth0 } from "@auth0/auth0-react";
+//import unitApi from "./services/uiApi";
 
 const App = () => {
-  
-  const  [user, setUser] = useState(null)
-  const [trainer, setTrainer]=useState(null)
-  const [errorMessage, setErrorMessage]=useState('')
-  const [workout, setWorkout]=useState(null)
+  const { isAuthenticated } = useAuth0()
+  const [Ouruser, setourUser] = useState('')
+  const [trainer, setTrainer] = useState(null)
+  // const [errorMessage, setErrorMessage]=useState('')
+  const [workout, setWorkout] = useState(null)
+
 
   return (
     <div>
-      <LoginForm setUser = {setUser} user = {user} setTrainer={setTrainer} trainer={trainer} errorMessage={errorMessage} setErrorMessage={setErrorMessage}/>
       {/*Below is the idea of how the profiles will be reached from the home screen*/}
-       <Router>
-         <div>
-           <Link to="/api/client">Client</Link>
-           <Link to="/api/trainer">Trainer</Link>
-           <Link to="/api/doWorkout">Workout</Link>
-          </div>
-         <Switch>
-           <Route path="/api/client">
-             <ClientProfile client={user}/>
-           </Route>
-           <Route path="/api/trainer">
-             <TrainerProfile trainer={trainer} client={user}/>
-           </Route>
-           <Route path="/api/doWorkout">
-            <DoWorkout client={user} workout={workout} setWorkout={setWorkout}/>
-           </Route>
-         </Switch>
-        </Router>
-      </div>
+      <Router>
+        <div>
+          {/*Below is the Login form using Auth0*/}
+          <Auth0Login setourUser={setourUser} setTrainer={setTrainer} trainer={trainer} />
+          {isAuthenticated && (
+            <><Link to="/api/client">Client</Link><Link to="/api/trainer">Trainer</Link><Link to="/api/doWorkout">Workout</Link></>
+          )}
+        </div>
+        <Switch>
+          <Route path="/api/client">
+            <ClientProfile setourUser={setourUser} Ouruser={Ouruser} />
+          </Route>
+          <Route path="/api/trainer">
+            <TrainerProfile setTrainer={setTrainer} trainer={trainer} Ouruser={Ouruser} />
+          </Route>
+          <Route path="/api/doWorkout">
+            <DoWorkout Ouruser={Ouruser} workout={workout} setWorkout={setWorkout} />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   )
 }
 
