a00145b1ccea06339d18fb376cbb28daa1928472
CYBERSPACE\DELL <ali.kazim@students.mq.edu.au>
2021-10-18 15:46:10 +0500

    Basic Auth0 component added.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {Auth0Provider} from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+  domain="dev-d-ju3bdt.us.auth0.com"
+  clientId="lDXFwHUEntIE0a8EoV6Mft6X5UaIVK0N"
+  redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById("root")
 );
