6be85d280db4231ab87ae60e630732c15185a713
blueatria <eric.korhonen@students.mq.edu.au>
2021-11-03 14:43:19 +1100

    add check for production
    add check to allow running on heroku

--- src/components/DoWorkout.js
+++ src/components/DoWorkout.js
@@ -6,6 +6,15 @@ import unitApi from "../services/uiApi";
 import axios from "axios";
 import Workout from "./Workout.jsx";
 
+//Checks if the production or development application is being run, and returns the correlating URL.
+const appurl = (() => {
+  if (process.env.NODE_ENV !== 'production')
+    return  "http://localhost:3001/";
+  else{
+      return "/";
+  }
+})();
+
 const DoWorkout = ({ Ouruser, setourUser, workout, setWorkout }) => {
   const [dataLoaded, setDataLoaded] = useState(false);
   const { user, isAuthenticated } = useAuth0();
@@ -13,7 +22,7 @@ const DoWorkout = ({ Ouruser, setourUser, workout, setWorkout }) => {
 
   useEffect(() => {
     const getData = async () => {
-      let result = await axios.get("http://localhost:3001/workout/");
+      let result = await axios.get(appurl + "workout/");
       console.log("Call Result", result);
       setWorkoutData(result.data);
       setDataLoaded(true);
