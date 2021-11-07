83a3754534b7902739896ce52cb0d9d4900e155b
CYBERSPACE\DELL <ali.kazim@students.mq.edu.au>
2021-11-07 15:07:17 +0500

    user validation added, front end

--- src/context/index.js
+++ src/context/index.js
@@ -1,14 +1,30 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Context = createContext();
 
 export function Provider(props) {
   const [workout, setWorkout] = useState([]);
+  const [token, setToken] = useState([]);
+
+  const { getAccessTokenSilently } = useAuth0()
+
 
   const getWorkoutData = () => {
+    //gets the token to attach with authorization headers
+    const getToken = async () => {
+      const token = await getAccessTokenSilently()
+      setToken(token)
+    }
+
+    getToken();
     axios
-      .get("/workout")
+      .get("/workout", {
+        headers: {
+          Authorization: `Bearer ${[token]}`
+        }
+      })
       .then((res) => {
         console.log(res);
         setWorkout(res.data);
@@ -18,7 +34,8 @@ export function Provider(props) {
       });
   };
 
-  useEffect(getWorkoutData, []);
+
+  useEffect(getWorkoutData, [token, getAccessTokenSilently]);
 
   const activeWorkout = workout.find(({ completed }) => !completed);
 
